008cd955d4ad8bb4ab1edc0bc79de315d6620617
Nitin Chawla <nitinchawla.emails@gmail.com>
2025-04-14 06:25:08 -0400

    Added buttons to suspend and unsuspend users

--- src/Components/BlockUserScreen.js
+++ src/Components/BlockUserScreen.js
@@ -1,17 +1,51 @@
 import React from "react";
 import { db } from "../firebase";
-import { collection, onSnapshot, query } from "firebase/firestore";
+import { collection, onSnapshot, query, addDoc, where, getDocs, deleteDoc } from "firebase/firestore";
 import { useState, useEffect } from 'react';
 
 export default function BlockUserScreen() {
 
     const [reportedUsers, setReportedUsers] = useState([]);
 
+    const suspendUser = async (item) => {
+
+        const suspendUsersRef = collection(db, "SuspendedUsers");
+        const q = query(suspendUsersRef, where("username", "==", item.user));
+        const snapshot = await getDocs(q);
+
+        if (snapshot.docs.length === 0) {
+            await addDoc(suspendUsersRef, {
+                username: item.user,
+            });
+            console.log("User suspended:", item.user);
+        }
+    }
+
+    const unsuspendUser = async (item) => {
+    
+      const suspendUsersRef = collection(db, "SuspendedUsers");
+      const q = query(suspendUsersRef, where("username", "==", item.user));
+      const snapshot = await getDocs(q);
+
+      if (snapshot.docs.length !== 0) {
+          await deleteDoc(snapshot.docs[0].ref);
+          console.log("User unsuspended:", item.user);
+      }
+    }
+
     function ListComponent({ items }) {
         return (
           <ul>
             {items.map((item) => (
-              <li key={item.user}>{item.user}</li>
+              <li key={item.user}>
+                {item.user}{" "}
+                <button onClick={() => suspendUser(item)}>
+                  Suspend User
+                </button>
+                <button onClick={() => unsuspendUser(item)}>
+                  Unsuspend User
+                </button>
+              </li>
             ))}
           </ul>
         );
